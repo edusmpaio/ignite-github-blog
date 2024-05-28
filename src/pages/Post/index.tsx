3fb55229c47f0bd5e1c5a6d0d6c3a00e7eb0c631
Eduardo Sampaio <edupcsampaio@gmail.com>
2024-05-27 22:00:53 -0300

    feat: add styles to post page

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,3 +1,76 @@
+import {
+  ArrowSquareOut,
+  CalendarDots,
+  CaretLeft,
+  ChatCircle,
+  GithubLogo,
+} from '@phosphor-icons/react'
+
+import {
+  PostContent,
+  PostInfos,
+  PostInfosFooter,
+  PostInfosHeader,
+} from './styles'
+import { Link } from 'react-router-dom'
+
 export function Post() {
-  return <h1>Post</h1>
+  return (
+    <>
+      <PostInfos>
+        <PostInfosHeader>
+          <Link to="/">
+            <CaretLeft size={16} weight="bold" />
+            <span>Voltar</span>
+          </Link>
+
+          <a
+            href="https://github.com/edusmpaio"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span>ver no github</span>
+            <ArrowSquareOut size={16} weight="bold" />
+          </a>
+        </PostInfosHeader>
+
+        <h1>JavaScript data types and data structures</h1>
+
+        <PostInfosFooter>
+          <span>
+            <GithubLogo size={20} />
+            cameronwll
+          </span>
+          <span>
+            <CalendarDots size={20} weight="fill" />
+            Há 1 dia
+          </span>
+          <span>
+            <ChatCircle size={20} weight="fill" />5 comentários
+          </span>
+        </PostInfosFooter>
+      </PostInfos>
+
+      <PostContent>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum nihil
+          accusamus deserunt totam earum rem nobis tempore impedit cumque! Aut
+          accusantium eius quidem ea dicta rem eligendi, quasi assumenda!
+          Exercitationem.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum nihil
+          accusamus deserunt totam earum rem nobis tempore impedit cumque! Aut
+          accusantium eius quidem ea dicta rem eligendi, quasi assumenda!
+          Exercitationem.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum nihil
+          accusamus deserunt totam earum rem nobis tempore impedit cumque! Aut
+          accusantium eius quidem ea dicta rem eligendi, quasi assumenda!
+          Exercitationem.
+        </p>
+      </PostContent>
+    </>
+  )
 }
---
+++ src/pages/Post/styles.ts
@@ -0,0 +1,61 @@
+import styled from 'styled-components'
+
+export const PostInfos = styled.div`
+  background: ${(props) => props.theme['gray-700']};
+  box-shadow: 0px 2px 28px 0px rgba(0, 0, 0, 0.2);
+  border-radius: 0.625rem;
+  padding: 2rem;
+
+  h1 {
+    font-size: 1.5rem;
+    color: ${(props) => props.theme['gray-50']};
+    margin-bottom: 0.5rem;
+  }
+`
+
+export const PostInfosHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  margin-bottom: 1.25rem;
+
+  a {
+    color: ${(props) => props.theme['blue-500']};
+    text-transform: uppercase;
+    font-size: 0.75rem;
+    display: flex;
+    align-items: center;
+    gap: 6px;
+    text-decoration: none;
+
+    span {
+      line-height: 120%;
+      font-weight: bold;
+    }
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`
+
+export const PostInfosFooter = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+
+  span {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    color: ${(props) => props.theme['gray-300']};
+
+    svg {
+      color: ${(props) => props.theme['gray-400']};
+    }
+  }
+`
+
+export const PostContent = styled.div`
+  padding: 2.5rem 2rem;
+`
